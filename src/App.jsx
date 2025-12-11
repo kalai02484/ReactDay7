38fa5a45e186a79282d2d5d916654d9538f6f797
kalai02484 <kalai02484@gmail.com>
2025-12-11 14:34:31 +0530

    Necessary Components and react-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 const App = () => {
   return (
-    <div>
-      <h1>React Day 7</h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
